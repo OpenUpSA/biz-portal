f4d57bd5136e757c058ceb94e314374b4ffe38c1
JD Bothma <jbothma@gmail.com>
2019-06-30 22:03:01 +0200

    Test single filter

--- biz_portal/apps/portal/tests/test_views.py
+++ biz_portal/apps/portal/tests/test_views.py
@@ -12,7 +12,7 @@ class BusinessSearchTestCase(TestCase):
         "test_views_businesses",
     ]
 
-    def test_search_business(self):
+    def test_list_business_no_filter(self):
         """Given three businesses, all are returned without any query or filters"""
         c = Client()
         response = c.get("/businesses/")
@@ -45,4 +45,33 @@ class BusinessSearchTestCase(TestCase):
             next(filter(lambda c: c["label"].startswith("Breda"), region_facet)).get(
                 "count"
             ),
+        )
+
+    def test_list_business_filter(self):
+        """Given three businesses, all are returned without any query or filters"""
+        c = Client()
+        response = c.get("/businesses/?sector=Accommodation and Food Services")
+        self.assertEqual(1, len(response.context["business_list"]))
+
+        # Facets
+        sector_facet = response.context["sector_business_counts"]
+        self.assertEqual(
+            1,
+            next(filter(lambda c: c["label"].startswith("Accom"), sector_facet)).get(
+                "count"
+            ),
+        )
+        self.assertEqual(
+            0, len(list(filter(lambda c: c["label"].startswith("Agric"), sector_facet)))
+        )
+
+        region_facet = response.context["region_business_counts"]
+        self.assertEqual(
+            1,
+            next(filter(lambda c: c["label"].startswith("Unkn"), region_facet)).get(
+                "count"
+            ),
+        )
+        self.assertEqual(
+            0, len(list(filter(lambda c: c["label"].startswith("Agric"), sector_facet)))
         )
