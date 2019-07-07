c3796792b73c1afbea27082243241fd6b9f4b9da
JD Bothma <jbothma@gmail.com>
2019-07-07 21:59:47 +0200

    Revert logging changes, doesn't seem like they changed anything

--- config/settings.py
+++ config/settings.py
@@ -240,19 +240,26 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": True,
     "formatters": {
-        "simple": {
-            "format": "%(asctime)s %(levelname)s %(module)s %(process)d %(thread)d %(message)s"
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
         }
     },
     "handlers": {
         "console": {
             "level": "DEBUG",
             "class": "logging.StreamHandler",
-            "formatter": "simple",
+            "formatter": "verbose",
         }
     },
-    "root": {"handlers": ["console"], "level": "ERROR"},
-    "loggers": {"django": {"level": "DEBUG" if DEBUG else "INFO"}},
+    "root": {"level": "INFO", "handlers": ["console"]},
+    "loggers": {
+        "django": {"level": "DEBUG" if DEBUG else "INFO"},
+        "": {"level": "DEBUG"},
+        # Errors logged by the SDK itself
+        "sentry_sdk": {"level": "ERROR", "handlers": ["console"], "propagate": False},
+        "django.security.DisallowedHost": {"level": "ERROR", "handlers": ["console"]},
+    },
 }
 
 
