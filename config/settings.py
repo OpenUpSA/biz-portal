d4da4dd7af9a4a56f49b79cb6d5ff092cd871ffe
JD Bothma <jbothma@gmail.com>
2019-07-07 18:45:46 +0200

    Try muni money logging config

--- config/settings.py
+++ config/settings.py
@@ -235,30 +235,24 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 # See https://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 
+# Logging
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": True,
     "formatters": {
-        "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s "
-            "%(process)d %(thread)d %(message)s"
+        "simple": {
+            "format": "%(asctime)s %(levelname)s %(module)s %(process)d %(thread)d %(message)s"
         }
     },
     "handlers": {
         "console": {
             "level": "DEBUG",
             "class": "logging.StreamHandler",
-            "formatter": "verbose",
+            "formatter": "simple",
         }
     },
-    "root": {"level": "INFO", "handlers": ["console"]},
-    "loggers": {
-        "django": {"level": "DEBUG" if DEBUG else "INFO"},
-        "": {"level": "DEBUG"},
-        # Errors logged by the SDK itself
-        "sentry_sdk": {"level": "ERROR", "handlers": ["console"], "propagate": False},
-        "django.security.DisallowedHost": {"level": "ERROR", "handlers": ["console"]},
-    },
+    "root": {"handlers": ["console"], "level": "ERROR"},
+    "loggers": {"django": {"level": "DEBUG" if DEBUG else "INFO"}},
 }
 
 
