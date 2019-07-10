589dcb02ea4a805ec437b1c6d9146d6a87fe9120
ktshep <ktshep@ktshep-pc.local>
2019-07-10 10:20:59 +0200

    change function name and fix code style

--- assets/js/business-detail.js
+++ assets/js/business-detail.js
@@ -1,10 +1,8 @@
 import {MDCDialog} from '@material/dialog';
 const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 
-const businessProfileModal = document.querySelector('#show').addEventListener('click', () => {
-  
+const openProfileModal = document.querySelector('#show').addEventListener('click', () => {
     dialog.open();
-      
   })
 
-export default businessProfileModal;
+export default openProfileModal;
