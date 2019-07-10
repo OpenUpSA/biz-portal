7ed4c50f491999e893b8caa499a0e3b2fcef32d2
ktshep <ktshep@ktshep-pc.local>
2019-07-10 11:10:53 +0200

    change name of file

--- assets/js/business-detail.js
+++
@@ -1,8 +0,0 @@
-import {MDCDialog} from '@material/dialog';
-const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
-
-const openProfileModal = document.querySelector('#show').addEventListener('click', () => {
-    dialog.open();
-  })
-
-export default openProfileModal;
