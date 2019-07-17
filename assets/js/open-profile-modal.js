8f67f3921b347f723f8e9af66f1026727c8cec2b
ktshep <ktshep@ktshep-pc.local>
2019-07-17 15:00:16 +0200

    fix JS error for modal

--- assets/js/open-profile-modal.js
+++ assets/js/open-profile-modal.js
@@ -1,8 +1,15 @@
 import {MDCDialog} from '@material/dialog';
-const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
 
-const openProfileModal = document.querySelector('#show').addEventListener('click', () => {
+const dialogNode = document.querySelector('.mdc-dialog');
+let openProfileModal;
+
+if (!!dialogNode) {
+  const dialog = new MDCDialog(dialogNode);
+
+  openProfileModal = document.querySelector('#show-business-menu').addEventListener('click', () => {
     dialog.open();
   })
+
+}
 
 export default openProfileModal;
