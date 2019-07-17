9e1437ed134d649148d89c1d6dc0172f51a2d854
JD Bothma <jbothma@gmail.com>
2019-07-17 18:03:47 +0200

    prefer supplied name, fall back to registered name

--- biz_portal/context_processors.py
+++ biz_portal/context_processors.py
@@ -1,5 +1,11 @@
+import logging
+
 from django.contrib.sites.models import Site
 from django.contrib.staticfiles import finders
+
+from .apps.portal import models
+
+logger = logging.getLogger(__name__)
 
 
 def current_site(request):
@@ -8,8 +14,12 @@ def current_site(request):
 
 def css_bundle(request):
     current_site = Site.objects.get_current(request)
-    muni_theme = f"biz-portal.bundle.{current_site.municipality.mdb_code}.css"
-    bundle = finders.find(muni_theme)
-    if not bundle:
-        muni_theme = "biz-portal.bundle.default.css"
-    return {"css_bundle": muni_theme}
+    try:
+        muni_theme = f"biz-portal.bundle.{current_site.municipality.mdb_code}.css"
+        bundle = finders.find(muni_theme)
+        if not bundle:
+            muni_theme = "biz-portal.bundle.default.css"
+        return {"css_bundle": muni_theme}
+    except models.Municipality.DoesNotExist:
+        logger.exception("Error when looking up site theme")
+    return {}
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#locale-paths
 LOCALE_PATHS = [ROOT_DIR.path("locale")]
 
+WERKZEUG_DEBUG_PIN = "off"
+
 
 # DATABASES
 # ------------------------------------------------------------------------------
