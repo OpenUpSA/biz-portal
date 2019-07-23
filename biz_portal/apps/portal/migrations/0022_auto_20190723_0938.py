67e2fce9be8f07ad039cfb6a50cb5da4597953ed
Jaron Rademeyer <jaronrademeyer@gmail.com>
2019-07-23 12:26:28 +0200

    * Update data migration to be able to handle different database states

--- biz_portal/apps/portal/migrations/0022_auto_20190723_0938.py
+++ biz_portal/apps/portal/migrations/0022_auto_20190723_0938.py
@@ -7,14 +7,24 @@ def migrate_empty_sectors_to_null(apps, schema_editor):
     Business = apps.get_model('portal', 'Business')
     Sector = apps.get_model('portal', 'Sector')
 
-    empty_sector_businesses = Business.objects.filter(sector__isnull=True)
-    unknown_sector = Sector.objects.get_or_create(label='unknown')
-    unknown_sector.label = 'Unknown sector'
-    unknown_sector.save()
+    unknown_sector_new = Sector.objects.filter(label='Unknown sector')
+    if unknown_sector_new.exists():
+        # If the new sector is already in the database, handle it appropriately
+        Sector.objects.filter(label='unknown').delete()
+        empty_sector_businesses = Business.objects.filter(sector__isnull=True)
+        for business in empty_sector_businesses:
+            business.sector = unknown_sector_new
+            business.save()
+    else:
+        # If the database doesn't have the new sector yet, handle it appropriately
+        empty_sector_businesses = Business.objects.filter(sector__isnull=True)
+        unknown_sector, created = Sector.objects.get_or_create(label='unknown')
+        unknown_sector.label = 'Unknown sector'
+        unknown_sector.save()
 
-    for business in empty_sector_businesses:
-        business.sector = unknown_sector
-        business.save()
+        for business in empty_sector_businesses:
+            business.sector = unknown_sector
+            business.save()
 
 
 def undo(apps, schema_editor):
